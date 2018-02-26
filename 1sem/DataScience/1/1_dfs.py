fefc48e9affd65a8aa7b1a6a743a84ee3872c556
earlyr1 <s02140286@stud.cs.msu.ru>
2018-02-26 20:28:48 +0300

    Finished dfs

--- 1sem/DataScience/1/1_dfs.py
+++ 1sem/DataScience/1/1_dfs.py
@@ -5,9 +5,9 @@ def dfs(direct, delta = 0):
 	lst = os.listdir(direct)
 	print "    " * delta + os.path.basename(direct) + ":"
 	for i in lst:
-		try:
+		if os.path.isdir(direct + '/' + i):
 			dfs(direct + "/" + i, delta + 1)
-		except Exception:
+		else:
 			print "    " * (delta+1) + i
 
-dfs(".")
+dfs("..")

6d6c97edb6e151f66f04b1e5afc6766ec8b656ed
earlyr1 <s02140286@stud.cs.msu.ru>
2018-03-29 17:02:57 +0300

    finish hw4

--- 1sem/C++1/homework/Radchenko/04/matrix_Class.cpp
+++ 1sem/C++1/homework/Radchenko/04/matrix_Class.cpp
@@ -8,13 +8,13 @@ using std::vector;
 class Row {
 	vector<int> vec;
 public:
-	Row(vector<int> vect) {
-		vec = vect;
-	}
-	Row(int m, int init) {
-		vec = vector<int>(m, init);
-	}
+	Row(vector<int> &vect): vec(vect) {};
+	Row(int m, int init): vec(m, init) {};
 	int& operator [](size_t ind) {
+		if (ind >= vec.size()) throw std::out_of_range("Out of column index");
+		return vec[ind];
+	}
+	const int& operator [](size_t ind) const {
 		if (ind >= vec.size()) throw std::out_of_range("Out of column index");
 		return vec[ind];
 	}
@@ -31,50 +31,41 @@ public:
 		m = M;
 		matr = vector<Row>(n, Row(m, init));
 	};
-	size_t getRows() {
+	size_t getRows() const {
 		return n;
 	}
-	size_t getColumns() {
+	size_t getColumns() const {
 		return m;
 	}
 	Row& operator[] (size_t i) {
 		if (i >= n) throw std::out_of_range("Out of row index");
 		return matr[i];
 	}
-	void operator *=(int C) {
+	const Row& operator[] (size_t i) const {
+		if (i >= n) throw std::out_of_range("Out of row index");
+		return matr[i];
+	}
+	Matrix& operator *=(int C) {
 		for (size_t i = 0; i < n; i++) {
 			for(size_t j = 0; j < m; j++) {
 				matr[i][j] *= C;
 			}
 		}
+		return *this;
 	}
-	friend bool operator== (Matrix &first, Matrix &other);
-	friend bool operator!= (Matrix &first, Matrix &other);
+	bool operator ==(const Matrix &other) const {
+		if ((*this).getRows() != other.getRows()) return false;
+		if ((*this).getColumns() != other.getColumns()) return false;
+		for (size_t i = 0; i < (*this).getRows(); i++) {
+			for(size_t j = 0; j < (*this).getColumns(); j++) {
+				if ((*this)[i][j] != other[i][j]) return false;
+			}
+		}
+		return true;
+	}
+
+	bool operator !=(const Matrix &other) const {
+		return !((*this) == other);
+	}
 };
 
-bool operator ==(Matrix &first, Matrix &other) {
-	if (first.getRows() != other.getRows()) return false;
-	if (first.getColumns() != other.getColumns()) return false;
-	for (size_t i = 0; i < first.getRows(); i++) {
-		for(size_t j = 0; j < first.getColumns(); j++) {
-			if (first[i][j] != other[i][j]) return false;
-		}
-	}
-	return true;
-	
-}
-
-bool operator !=(Matrix &first, Matrix &other)  {
-	return !(first == other);
-}
-
-int main() {
-	Matrix m1 = Matrix(3, 4);
-	Matrix m2 = Matrix(3, 4);
-	Matrix m3 = Matrix(4, 5, 1);
-	std::cout << (m1 == m2) << " " << (m2 == m3) << std::endl;
-	m3 *= 4;
-	m3[0][0] = 6;
-	std::cout << m3[0][1] << " " << m3[0][0] << std::endl;
-
-}
